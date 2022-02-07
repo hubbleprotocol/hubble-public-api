2a04823807f105ea201de6bf7c0c786b0fe6c603
peroxy <peroxy@users.noreply.github.com>
2022-02-07 12:49:19 +0100

    Add test

--- src/netlify/functions/test.ts
+++ src/netlify/functions/test.ts
@@ -1,24 +1,10 @@
 import { Handler } from '@netlify/functions';
 
-export const handler: Handler = (event, context) => {
-  console.log('hello world?');
+const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ test: 'hi' }),
+    body: JSON.stringify({ message: 'Hello World' }),
   };
-
-  // let env: ENV | undefined;
-  // if (event?.queryStringParameters?.env) {
-  //   env = event.queryStringParameters.env as ENV;
-  // }
-  // try {
-  //   if (env) {
-  //     return ok(getConfigByEnv(env));
-  //   }
-  //
-  //   return ok(HUBBLE_CONFIGS);
-  // } catch (e) {
-  //   console.error(e);
-  //   return internalError(e);
-  // }
 };
+
+export { handler };
