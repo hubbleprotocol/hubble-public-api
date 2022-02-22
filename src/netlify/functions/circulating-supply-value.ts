df541a7d124227b789e77a5c0118ce800bb28bfa
peroxy <peroxy@users.noreply.github.com>
2022-02-22 11:09:51 +0100

    Add circulating-supply and circulating-supply-value

--- src/netlify/functions/circulating-supply-value.ts
+++ src/netlify/functions/circulating-supply-value.ts
@@ -1,17 +1,20 @@
 import { Handler } from '@netlify/functions';
 import { parseFromQueryParams, ok } from '../../utils/apiUtils';
 import { BorrowingClient } from '../../services/hubble/BorrowingClient';
+import { OrcaPriceService } from '../../services/price/OrcaPriceService';
 
 /**
- * Get circulating supply of HBB (amount of HBB issued). This endpoint is required for external services like CoinGecko.
+ * Get circulating supply value of HBB (HBB issued * HBB price). This endpoint is required for external services like CoinMarketCap.
  */
 export const handler: Handler = async (event, context) => {
   const [web3Client, env, error] = parseFromQueryParams(event);
   if (web3Client && env) {
     const client = new BorrowingClient(web3Client.connection, env);
+    const orcaService = new OrcaPriceService();
     const hbbMint = await client.getHbbMintAccount();
-    const circulatingSupply = hbbMint.uiAmount as number;
-    return ok(circulatingSupply.toString(), false);
+    const hbbPrice = (await orcaService.getHbbPrice()).getRate().toNumber();
+    const circulatingSupplyValue = (hbbMint.uiAmount as number) * hbbPrice;
+    return ok(circulatingSupplyValue.toString(), false);
   }
   return error;
 };
