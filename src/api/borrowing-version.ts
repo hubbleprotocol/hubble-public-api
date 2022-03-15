ada5fc475dd7750a8c607eef391f8c9f31d5fa4b
peroxy <peroxy@users.noreply.github.com>
2022-03-15 10:55:17 +0100

    Read borrowing version from AWS

--- src/api/borrowing-version.ts
+++ src/api/borrowing-version.ts
@@ -1,9 +1,14 @@
-import { parseFromQueryParams, unprocessable } from '../utils/apiUtils';
+import { badGateway, internalError } from '../utils/apiUtils';
 import Router from 'express-promise-router';
 import { Request } from 'express';
 import EnvironmentQueryParams from '../models/api/EnvironmentQueryParams';
-import { Hubble } from '@hubbleprotocol/hubble-sdk';
 import { BorrowingVersionResponse } from '../models/api/BorrowingVersionResponse';
+import { ENV } from '../services/web3/client';
+import { getBorrowingVersionParameterName } from '../constants/hubble';
+import { getParameter } from '../utils/awsUtils';
+import { getAwsEnvironmentVariables } from '../services/environmentService';
+
+const awsEnv = getAwsEnvironmentVariables();
 
 /**
  * Get current borrowing market state version
@@ -12,13 +17,31 @@ const borrowingVersionRoute = Router();
 borrowingVersionRoute.get(
   '/',
   async (request: Request<never, string | BorrowingVersionResponse, never, EnvironmentQueryParams>, response) => {
-    const [web3Client, env, error] = parseFromQueryParams(request.query);
-    if (web3Client && env) {
-      const hubbleSdk = new Hubble(env, web3Client.connection);
-      const market = await hubbleSdk.getBorrowingMarketState();
-      response.send({ version: market.version });
+    let env: ENV = 'mainnet-beta';
+    if (request.query.env) {
+      env = request.query.env;
+    }
+    const parameterName = getBorrowingVersionParameterName(env);
+    const parameter = await getParameter(
+      parameterName,
+      awsEnv.AWS_ACCESS_KEY_ID,
+      awsEnv.AWS_SECRET_ACCESS_KEY,
+      awsEnv.AWS_REGION
+    );
+    const borrowingVersionValue = parameter.Parameter?.Value;
+    if (borrowingVersionValue) {
+      const borrowingVersion = parseInt(borrowingVersionValue);
+      if (isNaN(borrowingVersion)) {
+        const err = `Could not parse borrowing version value from AWS (has to be number): ${borrowingVersionValue}`;
+        console.error(err);
+        response.status(internalError).send(err);
+        return;
+      }
+      response.send({ version: borrowingVersion });
     } else {
-      response.status(unprocessable).send(error);
+      const err = 'Could not get borrowing version value from AWS';
+      console.error(err);
+      response.status(badGateway).send(err);
     }
   }
 );
