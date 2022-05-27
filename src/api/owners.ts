e710c40e08cb8175f843ec39360a7ca2ab5baa02
peroxy <peroxy@users.noreply.github.com>
2022-05-27 15:25:46 +0200

    Add redis locking to owners route

--- src/api/owners.ts
+++ src/api/owners.ts
@@ -3,15 +3,17 @@ import { LoanResponse } from '../models/api/LoanResponse';
 import EnvironmentQueryParams from '../models/api/EnvironmentQueryParams';
 import { ENV, Web3Client } from '../services/web3/client';
 import { tryGetPublicKeyFromString } from '../utils/tokenUtils';
-import { badRequest } from '../utils/apiUtils';
+import { badRequest, internalError } from '../utils/apiUtils';
 import { Hubble, UserMetadata } from '@hubbleprotocol/hubble-sdk';
 import Router from 'express-promise-router';
 import { getLoansFromUserVaults, LoansParameters } from './loans';
 import { getConfigByCluster } from '@hubbleprotocol/hubble-config';
 import { PythPrice, PythPriceService } from '../services/price/PythPriceService';
-import redis from '../services/redis/redis';
+import redis, { CacheExpiryType } from '../services/redis/redis';
 import { getOwnerRedisKey } from '../services/redis/keyProvider';
+import { PublicKey } from '@solana/web3.js';
 import { LOANS_EXPIRY_IN_SECONDS } from '../constants/redis';
+import logger from '../services/logger';
 
 const ownersRoute = Router();
 
@@ -22,34 +24,34 @@ ownersRoute.get(
   '/:pubkey/loans',
   async (request: Request<LoansParameters, LoanResponse[] | string, never, EnvironmentQueryParams>, response) => {
     let env: ENV = request.query.env ?? 'mainnet-beta';
-
-    let user = tryGetPublicKeyFromString(request.params.pubkey);
+    const user = tryGetPublicKeyFromString(request.params.pubkey);
     if (!user) {
       response.status(badRequest).send(`could not parse public key from: ${request.params.pubkey}`);
       return;
     }
-
     const key = getOwnerRedisKey(user, env);
-    const cached = await redis.getAndParseKey<LoanResponse[]>(key);
-    if (cached) {
-      response.send(cached);
-    } else {
-      let web3Client: Web3Client = new Web3Client(env);
-      const hubbleSdk = new Hubble(env, web3Client.connection);
-
-      const config = getConfigByCluster(env);
-      const pythService = new PythPriceService(web3Client, config);
-
-      const responses = await Promise.all([pythService.getTokenPrices(), hubbleSdk.getUserMetadatas(user)]);
-
-      const pythPrices: PythPrice[] = responses[0];
-      const userVaults: UserMetadata[] = responses[1];
-      const loans = getLoansFromUserVaults(userVaults, pythPrices);
-
+    try {
+      const loans = await redis.cacheFetchJson(key, () => getOwnerLoans(env, user), {
+        cacheExpiryType: CacheExpiryType.ExpireInSeconds,
+        cacheExpirySeconds: LOANS_EXPIRY_IN_SECONDS,
+      });
       response.send(loans);
-      await redis.saveAsJsonWithExpiry(key, loans, LOANS_EXPIRY_IN_SECONDS);
+    } catch (e) {
+      logger.error(e);
+      response.status(internalError).send('Could not get owner loans');
     }
   }
 );
+
+async function getOwnerLoans(env: ENV, user: PublicKey) {
+  let web3Client: Web3Client = new Web3Client(env);
+  const hubbleSdk = new Hubble(env, web3Client.connection);
+  const config = getConfigByCluster(env);
+  const pythService = new PythPriceService(web3Client, config);
+  const responses = await Promise.all([pythService.getTokenPrices(), hubbleSdk.getUserMetadatas(user)]);
+  const pythPrices: PythPrice[] = responses[0];
+  const userVaults: UserMetadata[] = responses[1];
+  return getLoansFromUserVaults(userVaults, pythPrices);
+}
 
 export default ownersRoute;
