177351e2f28b89c568e17066fcfa6e3ae8d78221
peroxy <peroxy@users.noreply.github.com>
2022-06-20 18:32:00 +0200

    Increase timeouts

--- src/services/redis/redis.ts
+++ src/services/redis/redis.ts
@@ -166,7 +166,7 @@ class RedisProvider {
         async () => {
           value = await get(key);
           if (value === undefined || value === null) {
-            await this._redlock.using([distributedLockKey], options.outerLockTimeoutMillis || 10_000, async () => {
+            await this._redlock.using([distributedLockKey], options.outerLockTimeoutMillis || 25_000, async () => {
               value = await get(key);
               if (value === undefined || value === null) {
                 value = await fetch();
@@ -177,7 +177,7 @@ class RedisProvider {
             });
           }
         },
-        { timeout: options.innerLockTimeoutMillis || 15_000 }
+        { timeout: options.innerLockTimeoutMillis || 30_000 }
       );
     }
     return value!;
